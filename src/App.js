d87b9020c448a3eff95c1fba73cc9c025542d1cf
LibirSoft <libir@outlook.com>
2021-05-06 11:56:27 +0300

    ders5.1 complate

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 import logo from "./logo.svg";
-import News from "./components/News";
 import "./App.css";
 
 import React, { Component } from "react";
-const news = [
-  {
-    key: 1,
-    title: "title 1 ",
-    description: "description 1",
-  },
-  {
-    key: 2,
-    title: "title 2 ",
-    description: "description 2",
-  },
-  {
-    key: 3,
-    title: "title 3 ",
-    description: "description 3",
-  },
-];
 
 export default class App extends Component {
-  addNews() {
-    console.log("app.js den geldim s.a");
-  }
+  state = {
+    name: "mehmet",
+  };
+  changename = () => {
+    this.setState({
+      name: "ahmet",
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1>{this.state.name}</h1>
           <img src={logo} className="App-logo" alt="logo" />
-          <News news={news} addNews={this.addNews} />
+          <h1>React Examples</h1>
+          <button onClick={this.changename}>name changer</button>
         </header>
       </div>
     );
